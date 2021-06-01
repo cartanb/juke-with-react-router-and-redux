4dbabd3f9851ad24d7d575af3ed6aa885d7d3db8
Brian Cartan <cartanb@gmail.com>
2021-06-01 17:19:01 -0400

    AllAlbums refactored

--- client/components/AllAlbums.js
+++ client/components/AllAlbums.js
@@ -1,35 +1,53 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchAlbums } from '../redux/albums';
 import { AlbumCard } from './';
 
-class AllAlbums extends React.Component {
-	componentDidMount() {
-		this.props.loadAlbums();
-	}
+const AllAlbums = () => {
+  const dispatch = useDispatch();
 
-	render() {
-		console.log(this.props);
+  useEffect(() => {
+    let isMounted = true;
 
-		return (
-			<div id='albums' className='row wrap'>
-				{this.props.albums.map(album => (
-					<Link key={album.id} to={`/albums/${album.id}`}>
-						<AlbumCard album={album} />
-					</Link>
-				))}
-			</div>
-		);
-	}
-}
+    if (isMounted) {
+      dispatch(fetchAlbums());
+    }
 
-const mapState = state => ({
-	albums: state.albums,
-});
+    return () => {
+      isMounted = false;
+    };
+  }, []);
 
-const mapDispatch = dispatch => ({
-	loadAlbums: () => dispatch(fetchAlbums()),
-});
+  const albums = useSelector((state) => state.albums);
 
-export default connect(mapState, mapDispatch)(AllAlbums);
+  return (
+    <div id="albums" className="row wrap">
+      {albums.map((album) => (
+        <Link key={album.id} to={`/albums/${album.id}`}>
+          <AlbumCard album={album} />
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+// class AllAlbums extends React.Component {
+//   componentDidMount() {
+//     this.props.loadAlbums();
+//   }
+
+//   render() {
+//     console.log(this.props);
+//   }
+// }
+
+// const mapState = (state) => ({
+//   albums: state.albums,
+// });
+
+// const mapDispatch = (dispatch) => ({
+//   loadAlbums: () => dispatch(fetchAlbums()),
+// });
+
+export default AllAlbums;
--- client/components/Player.js
+++ client/components/Player.js
@@ -9,100 +9,174 @@ import PlayPauseBtn from './PlayPauseBtn';
 const backIcon = 'fa fa-step-backward';
 const forwardIcon = 'fa fa-step-forward';
 
-class Player extends React.Component {
-	constructor(props) {
-		super(props);
-		this.setPreviousSong = this.setPreviousSong.bind(this);
-		this.setNextSong = this.setNextSong.bind(this);
-	}
-
-	// functions for song navigation
+const Player = (props) => {
 	setPreviousSong(currentAlbum, currentSong, audioRef) {
-		if (!currentAlbum.songs) return;
+    if (!currentAlbum.songs) return;
 
-		let previousIndex =
-			currentAlbum.songs.findIndex(song => song.id === currentSong.id) - 1;
+    let previousIndex =
+      currentAlbum.songs.findIndex((song) => song.id === currentSong.id) - 1;
 
-		if (previousIndex < 0) {
-			previousIndex = currentAlbum.songs.length - 1;
-		}
+    if (previousIndex < 0) {
+      previousIndex = currentAlbum.songs.length - 1;
+    }
 
-		const previousSong = currentAlbum.songs[previousIndex];
+    const previousSong = currentAlbum.songs[previousIndex];
 
-		this.props.setCurrentSong(previousSong);
-		handlePlayerBtnClick(audioRef, previousSong.audioUrl);
-		this.props.setPaused(audioRef.paused);
-	}
+    props.setCurrentSong(previousSong);
+    handlePlayerBtnClick(audioRef, previousSong.audioUrl);
+    props.setPaused(audioRef.paused);
+  }
 
 	setNextSong(currentAlbum, currentSong, audioRef) {
-		if (!currentAlbum.songs) return;
+    if (!currentAlbum.songs) return;
 
-		let nextIndex =
-			currentAlbum.songs.findIndex(song => song.id === currentSong.id) + 1;
+    let nextIndex =
+      currentAlbum.songs.findIndex((song) => song.id === currentSong.id) + 1;
 
-		if (nextIndex === currentAlbum.songs.length) {
-			nextIndex = 0;
-		}
+    if (nextIndex === currentAlbum.songs.length) {
+      nextIndex = 0;
+    }
 
-		const nextSong = currentAlbum.songs[nextIndex];
+    const nextSong = currentAlbum.songs[nextIndex];
 
-		this.props.setCurrentSong(nextSong);
-		handlePlayerBtnClick(audioRef, nextSong.audioUrl);
-		this.props.setPaused(audioRef.paused);
-	}
+    props.setCurrentSong(nextSong);
+    handlePlayerBtnClick(audioRef, nextSong.audioUrl);
+    props.setPaused(audioRef.paused);
+  }
 
-	render() {
-		const { currentAlbum, currentSong, audioRef, trackTime } = this.props;
+  const { currentAlbum, currentSong, audioRef, trackTime } = props;
+  return (
+    <div id="player-container">
+      <div id="player-controls">
+        <div id="current-album">
+          <img src={currentAlbum.artworkUrl} />
+          <span>
+            {currentAlbum.name}
+            {currentAlbum.name && ' | '}
+            {currentAlbum.artist && currentAlbum.artist.name}
+          </span>
+        </div>
+        <div className="row center">
+          <i
+            className={backIcon}
+            onClick={() =>
+              setPreviousSong(currentAlbum, currentSong, audioRef)
+            }
+          />
+          {/* play/pause btn requires a song prop, which is used to conditionally render the btn icon -- by passing currentSong here, we are asserting that this play/pause btn will always be the active one for any play/pause action */}
+          <PlayPauseBtn song={currentSong} />
+          <i
+            className={forwardIcon}
+            onClick={() =>
+              setNextSong(currentAlbum, currentSong, audioRef)
+            }
+          />
+        </div>
+        {/* audioRef's onTimeUpdate event handler makes trackTime available on the redux store's audio object */}
+        <div id="track-name-and-time">
+          {/* only show song name once track has loaded */}
+          <span id="track-name">{trackTime ? currentSong.name : ''}</span>
+          <span id="track-time">{trackTime}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-		return (
-			<div id='player-container'>
-				<div id='player-controls'>
-					<div id='current-album'>
-						<img src={currentAlbum.artworkUrl} />
-						<span>
-							{currentAlbum.name}
-							{currentAlbum.name && ' | '}
-							{currentAlbum.artist && currentAlbum.artist.name}
-						</span>
-					</div>
-					<div className='row center'>
-						<i
-							className={backIcon}
-							onClick={() =>
-								this.setPreviousSong(currentAlbum, currentSong, audioRef)
-							}
-						/>
-						{/* play/pause btn requires a song prop, which is used to conditionally render the btn icon -- by passing currentSong here, we are asserting that this play/pause btn will always be the active one for any play/pause action */}
-						<PlayPauseBtn song={currentSong} />
-						<i
-							className={forwardIcon}
-							onClick={() =>
-								this.setNextSong(currentAlbum, currentSong, audioRef)
-							}
-						/>
-					</div>
-					{/* audioRef's onTimeUpdate event handler makes trackTime available on the redux store's audio object */}
-					<div id='track-name-and-time'>
-						{/* only show song name once track has loaded */}
-						<span id='track-name'>{trackTime ? currentSong.name : ''}</span>
-						<span id='track-time'>{trackTime}</span>
-					</div>
-				</div>
-			</div>
-		);
-	}
+class Player extends React.Component {
+  constructor(props) {
+    super(props);
+    this.setPreviousSong = this.setPreviousSong.bind(this);
+    this.setNextSong = this.setNextSong.bind(this);
+  }
+
+  // functions for song navigation
+  setPreviousSong(currentAlbum, currentSong, audioRef) {
+    if (!currentAlbum.songs) return;
+
+    let previousIndex =
+      currentAlbum.songs.findIndex((song) => song.id === currentSong.id) - 1;
+
+    if (previousIndex < 0) {
+      previousIndex = currentAlbum.songs.length - 1;
+    }
+
+    const previousSong = currentAlbum.songs[previousIndex];
+
+    this.props.setCurrentSong(previousSong);
+    handlePlayerBtnClick(audioRef, previousSong.audioUrl);
+    this.props.setPaused(audioRef.paused);
+  }
+
+  setNextSong(currentAlbum, currentSong, audioRef) {
+    if (!currentAlbum.songs) return;
+
+    let nextIndex =
+      currentAlbum.songs.findIndex((song) => song.id === currentSong.id) + 1;
+
+    if (nextIndex === currentAlbum.songs.length) {
+      nextIndex = 0;
+    }
+
+    const nextSong = currentAlbum.songs[nextIndex];
+
+    this.props.setCurrentSong(nextSong);
+    handlePlayerBtnClick(audioRef, nextSong.audioUrl);
+    this.props.setPaused(audioRef.paused);
+  }
+
+  render() {
+    const { currentAlbum, currentSong, audioRef, trackTime } = this.props;
+
+    return (
+      <div id="player-container">
+        <div id="player-controls">
+          <div id="current-album">
+            <img src={currentAlbum.artworkUrl} />
+            <span>
+              {currentAlbum.name}
+              {currentAlbum.name && ' | '}
+              {currentAlbum.artist && currentAlbum.artist.name}
+            </span>
+          </div>
+          <div className="row center">
+            <i
+              className={backIcon}
+              onClick={() =>
+                this.setPreviousSong(currentAlbum, currentSong, audioRef)
+              }
+            />
+            {/* play/pause btn requires a song prop, which is used to conditionally render the btn icon -- by passing currentSong here, we are asserting that this play/pause btn will always be the active one for any play/pause action */}
+            <PlayPauseBtn song={currentSong} />
+            <i
+              className={forwardIcon}
+              onClick={() =>
+                this.setNextSong(currentAlbum, currentSong, audioRef)
+              }
+            />
+          </div>
+          {/* audioRef's onTimeUpdate event handler makes trackTime available on the redux store's audio object */}
+          <div id="track-name-and-time">
+            {/* only show song name once track has loaded */}
+            <span id="track-name">{trackTime ? currentSong.name : ''}</span>
+            <span id="track-time">{trackTime}</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapState = state => ({
-	audioRef: state.audio.audioRef,
-	trackTime: state.audio.trackTime,
-	currentAlbum: state.currentAlbum,
-	currentSong: state.currentSong,
+const mapState = (state) => ({
+  audioRef: state.audio.audioRef,
+  trackTime: state.audio.trackTime,
+  currentAlbum: state.currentAlbum,
+  currentSong: state.currentSong,
 });
 
-const mapDispatch = dispatch => ({
-	setCurrentSong: song => dispatch(setCurrentSong(song)),
-	setPaused: status => dispatch(setPaused(status)),
+const mapDispatch = (dispatch) => ({
+  setCurrentSong: (song) => dispatch(setCurrentSong(song)),
+  setPaused: (status) => dispatch(setPaused(status)),
 });
 
 export default connect(mapState, mapDispatch)(Player);
